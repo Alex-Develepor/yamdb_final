03f379df90aa6bfbc3365582920ebf44ae940a0d
Дворецкий Александр <Ub1ka@yandex.ru>
2022-09-11 14:35:17 +0300

    1st

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from users.views import UserViewSet, get_jwt_token, signup
+
 from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
                     ReviewViewSet, TitleViewSet)
 
